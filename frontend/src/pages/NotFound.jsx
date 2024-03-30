28b8146ec44391d19a12f1f8721a4507240fbe2d
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-03-30 10:35:54 +0530

    Created a form dynamic to login and register

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 function NotFound() {
     return (
-        <div>NotFound</div>
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The page you&apos;re looking for does not exist!</p>
+        </div>
     )
 }
 export default NotFound
