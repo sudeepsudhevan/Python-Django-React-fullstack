2d6ce466b184c4f4a3959df25db7819248c6d5e6
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-03-30 09:31:08 +0530

    Created route of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,36 @@
-//import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Register from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
 
 
-function App()
-{
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
 
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
+function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
