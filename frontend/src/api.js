d076866b298f9169df0c13dbcbf545ca772fbc64
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-03-30 16:53:59 +0530

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,22 +1,21 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "/choreo-apis/pythondjangoreact/backend/rest-api-be2/v1.0"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 })
 
 api.interceptors.request.use(
-    (config) =>
-    {
+    (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token)
-        {
+        if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
         return config
     },
-    (error) =>
-    {
+    (error) => {
         return Promise.reject(error)
     }
 )
